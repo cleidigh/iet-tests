0064e214dbf094c686a23510c5593d3591584560
Christopher Leidigh <cleidigh@Gmail.com>
2020-05-18 19:38:09 -0400

    workingMessages 1 400db no update

--- src/content/iet-test1.js
+++ src/content/iet-test1.js
@@ -7,7 +7,9 @@ var { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm');
 var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
 var { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
 
-
+var msgfolderArray = [];
+var folderArray = [];
+var msgCount = 0;
 
 function createFoldersT1() {
 
@@ -40,31 +42,66 @@ async function createFolderStructureT2() {
 
 	var osFolder = promptImportDirectory();
 
-	var dirs = dirWalk(osFolder.file);
+	// var dirs = dirWalk(osFolder.file);
+	var dirs = await dirWalk2(osFolder.file.path);
+	console.debug('AfterWalk');
 	for (let index = 0; index < dirs.length; index++) {
 		const element = dirs[index];
-		console.debug(element);
+		console.debug(element.path.length + ' : ' + element.path);
 
 	}
-	test_cycles = Preferences.get("extensions.iet-ng-tests.test_cycles").value;
+	// return;
+	var test_cycles = Preferences.get("extensions.iet-ng-tests.test_cycles").value;
 	console.debug(test_cycles);
 
 	for (let index = 0; index < test_cycles; index++) {
 		createFolderStructure(osFolder, osFolder.file.path, msgFolder, dirs);
-		await resetSelectedFolder(msgFolder);
-	
+		if (test_cycles > 1) {
+			await resetSelectedFolder(msgFolder);
+		}
 	}
+}
+
+async function ImportEMLStructuredExt() {
+	msgCount = 0;
+	let msgFolder = GetSelectedMsgFolders()[0];
+
+	var osFolder = promptImportDirectory();
+
+	let startTime = new Date();
+
+	var dirs = await dirWalk2(osFolder.file.path);
+	// console.debug('AfterWalk');
+	// for (let index = 0; index < dirs.length; index++) {
+	// 	const element = dirs[index];
+	// 	console.debug(element.path.length + ' : ' + element.path);
+	// }
+	// return;
+	var test_cycles = Preferences.get("extensions.iet-ng-tests.test_cycles").value;
+	console.debug(test_cycles);
+
+	createFolderStructure(osFolder, osFolder.file.path, msgFolder, dirs);
+
+	// for (let index = 0; index < folderArray.length; index++) {
+	// 	const element = folderArray[index];
+	// 	console.debug(element);
+	// }
+	
+	await importOSFolderMessages(msgFolder, msgfolderArray, dirs);
+	let stepTime = new Date();
+	console.debug('Import ElapsedTime: ' + (stepTime - startTime) / 1000 + ' sec');
+
+
 }
 
 function createFolderStructure(osFolder, rootOSFolder, msgFolder, dirs) {
 	let startTime = new Date();
 
 	var curParentFolder = null;
-	var msgfolderArray = [];
 
 	// console.debug(rootOSFolder);
-	var folderArray = dirs.map(d => {
-		let da = d.split(rootOSFolder + '\\')[1];
+	folderArray = dirs.map(d => {
+		let da = d.path.split(rootOSFolder + '\\')[1];
 		return da;
 	});
 
@@ -77,21 +114,37 @@ function createFolderStructure(osFolder, rootOSFolder, msgFolder, dirs) {
 		let newFolderName = OS.Path.basename(folderArray[index]);
 		let OSDirPath = OS.Path.dirname(folderArray[index]);
 
-		if (OSDirPath === '.') {
-			curParentFolder = msgFolder;
-			curParentFolder.createSubfolder(newFolderName, msgWindow);
-			// console.debug('created: '+ newFolderName);
-			msgfolderArray[index] = curParentFolder.getChildNamed(newFolderName);
-		} else {
-			curParentFolder = msgfolderArray[folderArray.indexOf(OSDirPath)];
-			curParentFolder.createSubfolder(newFolderName, msgWindow);
-			// console.debug('created subfolder: '+ newFolderName);
-			msgfolderArray[index] = curParentFolder.getChildNamed(newFolderName);
+		try {
+			if (OSDirPath === '.') {
+				curParentFolder = msgFolder;
+				curParentFolder.createSubfolder(newFolderName, msgWindow);
+				// console.debug('created: '+ newFolderName);
+				msgfolderArray[index] = curParentFolder.getChildNamed(newFolderName);
+			} else {
+				curParentFolder = msgfolderArray[folderArray.indexOf(OSDirPath)];
+				curParentFolder.createSubfolder(newFolderName, msgWindow);
+				// console.debug('created subfolder: '+ newFolderName);
+				msgfolderArray[index] = curParentFolder.getChildNamed(newFolderName);
+			}
+				
+		} catch (error) {
+			console.debug('Error '+ error);
+			console.debug(newFolderName);
 		}
+		
 	}
 	let stepTime = new Date();
 	console.debug('CreateFolders ElapsedTime: ' + (stepTime - startTime) / 1000 + ' sec');
 
+}
+
+async function importOSFolderMessages(msgFolder, msgfolderArray, OSFolderArray) {
+
+	for (let index = 0; index < OSFolderArray.length; index++) {
+		const folder = OSFolderArray[index].path;
+
+		await messageFolderImport(msgFolder, msgfolderArray[index], folder);
+	}
 }
 
 // 100 folders, 100msg, 10k size - 58s
@@ -256,5 +309,193 @@ function dirWalk(dir) {
 	}
 	return allDirs;
 
+}
+
+async function dirWalk2(dirPath) {
+	var iterator = new OS.File.DirectoryIterator(dirPath);
+	var subdirs = [];
+
+	// Iterate through the directory
+	let p = iterator.forEach(
+		function onEntry(entry) {
+			if (entry.isDir) {
+				// console.debug(entry.name);
+				// console.debug(entry.path);
+				subdirs.push(entry);
+				// subdirs.push(entry.path);
+			} else {
+				// console.debug('file  ' + entry.name);
+			}
+		}
+	);
+
+	return p.then(
+		async function onSuccess() {
+			iterator.close();
+			// console.debug('dirs: ' + subdirs.map(d => d.name + ' '));
+
+			for (const dir of subdirs) {
+				// console.debug('subWalk '+ dir.name);
+				let dirs = await dirWalk2(dir.path);
+				subdirs = subdirs.concat(dirs);
+				// console.debug('accumulated dirs: ' + subdirs.map(d => d.name + ' '));
+			}
+
+			return subdirs;
+		},
+		function onFailure(reason) {
+			iterator.close();
+			throw reason;
+		}
+	);
+
+}
+
+
+async function messageFolderImport(rootFolder, msgFolder, dirPath) {
+	var iterator = new OS.File.DirectoryIterator(dirPath);
+	var messageEntries = [];
+	var fileArray = "";
+	var test_updateCycle = Preferences.get("extensions.iet-ng-tests.test_updatecycle").value;
+	var test_updateCount = Preferences.get("extensions.iet-ng-tests.test_updatecount").value;
+	var test_pawaitCycle = Preferences.get("extensions.iet-ng-tests.test_pawaitcycle").value;
+	var test_usecfawait = Preferences.get("extensions.iet-ng-tests.test_usecfawait").value;
+
+
+	msgFolder = msgFolder.QueryInterface(Ci.nsIMsgLocalMailFolder);
+	
+	// console.debug('Folder ' + msgFolder.name);
+	// Iterate through the directory
+	let p = iterator.forEach(
+		async function onEntry(entry) {
+			if (!entry.isDir) {
+				if (msgCount++ % test_updateCycle === 0) {
+					rootFolder.ForceDBClosed();
+					if (test_usecfawait) {
+						msgFolder.parent.updateFolder(msgWindow);
+					}
+					console.debug('update');
+				}
+		
+				// console.debug(entry.name);
+				console.debug(msgCount + '  : ' + entry.path);
+				if (fileArray === "") {
+					fileArray = await readFile1(entry.path);
+					
+				}
+				fileArray = fixFile(fileArray, msgFolder);
+				// console.debug(fileArray);
+				try {
+					msgFolder.addMessage(fileArray);
+					
+				} catch (e) {
+					console.debug(msgCount + '  AdMessageError ' + e);
+					console.debug(entry.path);
+				}
+				messageEntries.push(entry);
+			} else {
+				// console.debug('folder entry  ' + entry.name);
+			}
+		}
+	);
+
+	return p.then(
+		async function onSuccess() {
+			iterator.close();
+			// console.debug('IteratorSuccess');
+			return;
+		},
+		function onFailure(reason) {
+			iterator.close();
+			throw reason;
+		}
+	);
+
+}
+
+async function readFile1(filePath) {
+
+	let decoder = new TextDecoder();        // This decoder can be reused for several reads
+	let promise = OS.File.read(filePath); // Read the complete file as an array
+	promise = promise.then(
+		function onSuccess(array) {
+
+			// console.debug('file okay ');
+			// console.debug('Size ' + array.length);
+			return decoder.decode(array);        // Convert this array to a text
+		},
+
+		function onRejected(reason) {
+			console.debug('FileRead Error: ' + reason);
+			return (reason);
+		}
+	);
+	return promise;
+}
+
+
+function fixFile(data, msgFolder) {
+	// Fix and transform data
+
+	// var msgFolder = GetSelectedMsgFolders()[0];
+	// msgFolder = msgFolder.QueryInterface(Ci.nsIMsgLocalMailFolder);
+
+	// strip off the null characters, that break totally import and display
+	data = data.replace(/\x00/g, "");
+	var now = new Date;
+	var nowString;
+
+	try {
+		nowString = now.toString().match(/.+:\d\d/);
+		nowString = nowString.toString().replace(/\d{4} /, "");
+		nowString = nowString + " " + now.getFullYear();
+	} catch (e) {
+		nowString = now.toString().replace(/GMT.+/, "");
+	}
+
+	var top = data.substring(0, 2000);
+
+	// Fix for crazy format returned by Hotmail view-source
+	if (top.match(/X-Message-Delivery:.+\r?\n\r?\n/) || top.match(/X-Message-Info:.+\r?\n\r?\n/))
+		data = data.replace(/(\r?\n\r?\n)/g, "\n");
+
+	// Fix for some not-compliant date headers
+	if (top.match(/Posted-Date\:/))
+		data = data.replace("Posted-Date:", "Date:");
+	if (top.match(/X-OriginalArrivalTime:.+\r?\n\r?\n/))
+		data = data.replace("X-OriginalArrivalTime:", "Date:");
+
+	// Some eml files begin with "From <something>"
+	// This causes that Thunderbird will not handle properly the message
+	// so in this case the first line is deleted
+	data = data.replace(/^From\s+.+\r?\n/, "");
+
+	// Prologue needed to add the message to the folder
+	var prologue = "From - " + nowString + "\n"; // The first line must begin with "From -", the following is not important
+	// If the message has no X-Mozilla-Status, we add them to it
+	// cleidigh - correct logic conversion
+	if (!data.includes("X-Mozilla-Status"))
+		prologue = prologue + "X-Mozilla-Status: 0000\nX-Mozilla-Status2: 00000000\n";
+	else {
+	// else if (IETprefs.getBoolPref("extensions.importexporttoolsng.reset_mozilla_status")) {
+		// Reset the X-Mozilla status
+		data = data.replace(/X-Mozilla-Status: \d{4}/, "X-Mozilla-Status: 0000");
+		data = data.replace(/X-Mozilla-Status2: \d{8}/, "X-Mozilla-Status2: 00000000");
+	}
+	// If the message has no X-Account-Key, we add it to it, taking it from the account selected
+	// cleidigh - correct logic conversion
+	if (!data.includes("X-Account-Key")) {
+		var myAccountManager = Cc["@mozilla.org/messenger/account-manager;1"]
+			.getService(Ci.nsIMsgAccountManager);
+		var myAccount = myAccountManager.FindAccountForServer(msgFolder.server);
+		prologue = prologue + "X-Account-Key: " + myAccount.key + "\n";
+	}
+
+	// fix this cleidigh
+	// data = IETescapeBeginningFrom(data);
+	// Add the prologue to the EML text
+	data = prologue + data + "\n";
+
+	return data;
 }
 
